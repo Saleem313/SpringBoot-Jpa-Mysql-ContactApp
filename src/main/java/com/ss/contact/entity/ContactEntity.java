4abe04a9c1544a7300a770a32052b157a5cf04b2
Saleem <saleems77866@gmail.com>
2021-08-12 13:31:43 +0530

    made class as Serializable

--- src/main/java/com/ss/contact/entity/ContactEntity.java
+++ src/main/java/com/ss/contact/entity/ContactEntity.java
@@ -8,12 +8,18 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @Data
 @Entity
 @Table(name="contact_master")
-public class ContactEntity {
+@AllArgsConstructor
+@NoArgsConstructor
+public class ContactEntity implements Serializable {
   @Id
   @Column(name="Contact_Id")
  // @GeneratedValue(strategy = GenerationType.IDENTITY)
